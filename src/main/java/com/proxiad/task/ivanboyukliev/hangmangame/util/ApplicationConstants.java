9eb4162a513a5cbf4ea9530663a7cb9a5970137d
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-12-14 17:18:57 +0200

    Added new constants related to error messages.

--- src/main/java/com/proxiad/task/ivanboyukliev/hangmangame/util/ApplicationConstants.java
+++ src/main/java/com/proxiad/task/ivanboyukliev/hangmangame/util/ApplicationConstants.java
@@ -5,4 +5,7 @@ public final class ApplicationConstants {
   public static final int INITIAL_TRIES = 6;
   public static final String UNKNOWN_LETTER_SYMBOL = "_";
   public static final int BONUS_TRIES = 2;
+  public static final String INVALID_USR_INPUT_LEN =
+      "User is supposed to enter single character only!";
+  public static final String INVALID_USR_INPUT_TYPE = "User is supposed to enter letters only!";
 }
