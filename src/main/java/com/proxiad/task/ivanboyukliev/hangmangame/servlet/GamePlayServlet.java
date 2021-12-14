b49e61108ab6ea13b8105fe31ecb573f8f4865a6
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-12-14 17:19:44 +0200

    Major re-factoring - user input validation was transfered to service layer.

--- src/main/java/com/proxiad/task/ivanboyukliev/hangmangame/servlet/GamePlayServlet.java
+++ src/main/java/com/proxiad/task/ivanboyukliev/hangmangame/servlet/GamePlayServlet.java
@@ -1,9 +1,7 @@
 package com.proxiad.task.ivanboyukliev.hangmangame.servlet;
 
 import java.io.IOException;
-import java.util.Optional;
 import com.proxiad.task.ivanboyukliev.hangmangame.domain.GameSession;
-import com.proxiad.task.ivanboyukliev.hangmangame.exception.InvalidGameSessionException;
 import com.proxiad.task.ivanboyukliev.hangmangame.service.GameSessionService;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
@@ -25,16 +23,10 @@ public class GamePlayServlet extends HttpServlet {
   protected void doGet(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
 
-    String gameId = getGameId(req);
+    GameSession currentGameSession =
+        gameSessionService.reloadGame(getServletContext(), req.getRequestURI());
 
-    Optional<GameSession> currentGameSession = checkForGameSessionExistence(gameId);
-
-    if (currentGameSession.isEmpty()) {
-      throw new InvalidGameSessionException(
-          "Game session with ID : " + gameId + " does not exist!");
-    }
-
-    req.setAttribute("gameSessionObj", currentGameSession.get());
+    req.setAttribute("gameSessionObj", currentGameSession);
     req.getRequestDispatcher("/hangmanMainPage.jsp").forward(req, resp);
   }
 
@@ -42,40 +34,28 @@ public class GamePlayServlet extends HttpServlet {
   protected void doPost(HttpServletRequest req, HttpServletResponse resp)
       throws ServletException, IOException {
 
-    String gameId = getGameId(req);
+    String userGuess = req.getParameter("enteredLetter").toLowerCase();
+    String requestUri = req.getRequestURI();
+    GameSession currentGameSession =
+        gameSessionService.makeTry(getServletContext(), requestUri, userGuess);
 
-    Optional<GameSession> currentGameSession = checkForGameSessionExistence(gameId);
-
-    if (currentGameSession.isEmpty()) {
-      throw new InvalidGameSessionException("Game session with ID : " + gameId + "does not exist!");
-    }
-
-    GameSession currentGameSessionObj = currentGameSession.get();
-    char receivedLetter = req.getParameter("enteredLetter").toLowerCase().charAt(0);
-
-    gameSessionService.makeTry(currentGameSessionObj, receivedLetter);
-
-    if (currentGameSessionObj.getLettersToGuessLeft() == 0) {
-      req.getSession().setAttribute("result", "Well Done! You have seccessfully guessed the word!");
+    if (currentGameSession.getLettersToGuessLeft() == 0) {
+      req.getSession().setAttribute("result", "Well Done! You have seccessfully guessed the word [ "
+          + currentGameSession.getWordToGuess() + " ]");
+      getServletContext().removeAttribute(currentGameSession.getGameId());
       resp.sendRedirect("/hangman-game/finalResultPage.jsp");
       return;
     }
 
-    if (currentGameSessionObj.getTriesLeft() == 0) {
-      req.getSession().setAttribute("result", "Game Over! No more tries left!");
+    if (currentGameSession.getTriesLeft() == 0) {
+      req.getSession().setAttribute("result", "Game Over! No more tries left! The word was [ "
+          + currentGameSession.getWordToGuess() + " ]");
+      getServletContext().removeAttribute(currentGameSession.getGameId());
       resp.sendRedirect("/hangman-game/finalResultPage.jsp");
       return;
     }
 
-    resp.sendRedirect("/hangman-game/games/" + gameId);
+    resp.sendRedirect("/hangman-game/games/" + currentGameSession.getGameId());
   }
 
-  private String getGameId(HttpServletRequest req) {
-    String uri = req.getRequestURI().substring(1);
-    return uri.substring(uri.indexOf("/", uri.indexOf("/") + 1) + 1, uri.length());
-  }
-
-  private Optional<GameSession> checkForGameSessionExistence(String gameId) {
-    return Optional.ofNullable((GameSession) getServletContext().getAttribute(gameId));
-  }
 }
