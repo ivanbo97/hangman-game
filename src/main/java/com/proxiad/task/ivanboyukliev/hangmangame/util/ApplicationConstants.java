6c9357d226629a4844765f94d979991579c5bc55
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-12-14 08:59:00 +0200

    This class stores app related data.

---
+++ src/main/java/com/proxiad/task/ivanboyukliev/hangmangame/util/ApplicationConstants.java
@@ -0,0 +1,8 @@
+package com.proxiad.task.ivanboyukliev.hangmangame.util;
+
+public final class ApplicationConstants {
+
+  public static final int INITIAL_TRIES = 6;
+  public static final String UNKNOWN_LETTER_SYMBOL = "_";
+  public static final int BONUS_TRIES = 2;
+}
