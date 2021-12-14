a8fc35a925f08ed3c99a2264c79537641344be63
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-12-14 09:10:25 +0200

    Class for configuring component scan for spring beans.

---
+++ src/main/java/com/proxiad/task/ivanboyukliev/hangmangame/springconfig/SpringContainerConfigurator.java
@@ -0,0 +1,10 @@
+package com.proxiad.task.ivanboyukliev.hangmangame.springconfig;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+
+@Configuration
+@ComponentScan("com.proxiad.task.ivanboyukliev.hangmangame")
+public class SpringContainerConfigurator {
+
+}
