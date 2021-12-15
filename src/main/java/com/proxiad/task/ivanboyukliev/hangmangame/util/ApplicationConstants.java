3cd46fdecfc18d311919d36ffe2a0a310f43640b
Ivan Boyukliev <ivanboukliev@abv.bg>
2021-12-15 09:37:48 +0200

    Added application end-points as string contants.

--- src/main/java/com/proxiad/task/ivanboyukliev/hangmangame/util/ApplicationConstants.java
+++ src/main/java/com/proxiad/task/ivanboyukliev/hangmangame/util/ApplicationConstants.java
@@ -8,4 +8,11 @@ public final class ApplicationConstants {
   public static final String INVALID_USR_INPUT_LEN =
       "User is supposed to enter single character only!";
   public static final String INVALID_USR_INPUT_TYPE = "User is supposed to enter letters only!";
+
+  // Application end-points
+
+  public static final String APP_BASE_URL = "/hangman-game";
+  public static final String START_NEW_GAME_URL = "/games";
+  public static final String GAME_PLAY_URL = "/games/*";
+
 }
