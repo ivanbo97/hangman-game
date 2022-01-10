d39d3ae316466b9d7e423de7aaa6bb990337ec39
Ivan Boyukliev <ivanboukliev@abv.bg>
2022-01-10 17:24:51 +0200

    Added test case for added encoding functionality.

--- src/test/java/com/proxiad/task/ivanboyukliev/hangmangame/service/GameSessionTest.java
+++ src/test/java/com/proxiad/task/ivanboyukliev/hangmangame/service/GameSessionTest.java
@@ -1,9 +1,11 @@
 package com.proxiad.task.ivanboyukliev.hangmangame.service;
 
 import static com.proxiad.task.ivanboyukliev.hangmangame.service.ApplicationConstants.BONUS_TRIES;
+import static com.proxiad.task.ivanboyukliev.hangmangame.service.ApplicationConstants.SECRET_ENCODE_VAL;
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.junit.jupiter.params.ParameterizedTest.ARGUMENTS_PLACEHOLDER;
 import java.util.stream.Stream;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
@@ -29,4 +31,19 @@ class GameSessionTest {
     return Stream.of(Arguments.of("stack", "s___k"), Arguments.of("kilowatt", "k_____tt"),
         Arguments.of("interface", "i__e____e"));
   }
+
+  @Test
+  void encodeLettersToBeGuessedTest() {
+
+    // given
+    GameSession gameSession = new GameSession("interface");
+    String encodedLettersToGuess = gameSession.getLettersToBeGuessed();
+
+    // when,then
+    for (char encodeLetter : encodedLettersToGuess.toCharArray()) {
+      char decodedLetter = (char) (encodeLetter + SECRET_ENCODE_VAL);
+      assertThat(gameSession.getWordToGuess().contains(String.valueOf(decodedLetter))).isTrue();
+    }
+  }
+
 }
