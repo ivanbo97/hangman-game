5b74bcdb0677ce63d93fd570f812ff79c8b7e4a5
Ivan Boyukliev <ivanboukliev@abv.bg>
2022-01-18 16:48:41 +0200

    Class for initiating spring boot app.

---
+++ src/main/java/com/proxiad/hangmangame/HangmanApplication.java
@@ -0,0 +1,11 @@
+package com.proxiad.hangmangame;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HangmanApplication {
+  public static void main(String[] args) {
+    SpringApplication.run(HangmanApplication.class, args);
+  }
+}
