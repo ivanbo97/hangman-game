e5e0c31979d681a14a544f96598e7a28f4f4cad9
Ivan Boyukliev <ivanboukliev@abv.bg>
2022-02-02 14:00:42 +0200

    Entity class for string retrieved word from DB.

---
+++ src/main/java/com/proxiad/hangmangame/model/HangmanWord.java
@@ -0,0 +1,28 @@
+package com.proxiad.hangmangame.model;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Id;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+import com.proxiad.hangmangame.HangmanApplication;
+
+@Entity
+@Table(name = "game_word")
+public class HangmanWord {
+
+  @Id @NotNull private Integer id;
+
+  @Column(name = "content")
+  private String content;
+
+  public HangmanWord() {}
+
+  public HangmanWord(String content) {
+    this.content = content;
+  }
+
+  public String getContent() {
+    return content;
+  }
+}
