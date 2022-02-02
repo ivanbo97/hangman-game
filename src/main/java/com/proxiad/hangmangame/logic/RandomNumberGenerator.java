47f0b3349a32754b0fcb6fc3dd96e80d5f2e317a
Ivan Boyukliev <ivanboukliev@abv.bg>
2022-02-02 14:00:56 +0200

    new upper and lower limits

--- src/main/java/com/proxiad/hangmangame/logic/RandomNumberGenerator.java
+++ src/main/java/com/proxiad/hangmangame/logic/RandomNumberGenerator.java
@@ -1,9 +1,9 @@
 package com.proxiad.hangmangame.logic;
 
 public class RandomNumberGenerator {
-  public static final int MAX_NUMBER = 6;
+  public static final int MAX_NUMBER = 8;
 
-  public static final int MIN_NUMBER = 0;
+  public static final int MIN_NUMBER = 1;
 
   public static int generateRandomNumber() {
     return (int) ((Math.random() * (MAX_NUMBER - MIN_NUMBER)) + MIN_NUMBER);
