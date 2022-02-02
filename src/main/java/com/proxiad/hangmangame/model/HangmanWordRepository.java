fabe28c807181e866144cb9b5fca0e357bfd6538
Ivan Boyukliev <ivanboukliev@abv.bg>
2022-02-02 13:59:30 +0200

    Repository interface for retrieving words from DB.

--- src/main/java/com/proxiad/hangmangame/model/HangmanWordRepository.java
+++ src/main/java/com/proxiad/hangmangame/model/HangmanWordRepository.java
@@ -2,30 +2,11 @@ package com.proxiad.hangmangame.model;
 
 import java.util.ArrayList;
 import java.util.List;
+import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 import com.proxiad.hangmangame.logic.RandomNumberGenerator;
 
 @Repository
-public class WordRepository {
-
-  private List<String> dictionary;
-
-  public WordRepository() {
-    dictionary = new ArrayList<>();
-    populateWithWords();
-  }
-
-  private void populateWithWords() {
-    dictionary.add("kilobyte");
-    dictionary.add("kilowatt");
-    dictionary.add("stack");
-    dictionary.add("localhost");
-    dictionary.add("interface");
-    dictionary.add("class");
-    dictionary.add("lambda");
-  }
-
-  public String getWord() {
-    return dictionary.get(RandomNumberGenerator.generateRandomNumber());
-  }
+public interface HangmanWordRepository extends JpaRepository<HangmanWord, Integer> {
+  HangmanWord getWordById(Integer id);
 }
