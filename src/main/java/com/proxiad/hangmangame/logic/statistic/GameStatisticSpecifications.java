81aff6e87bdf0917b8cf594818ebbf8257581f39
Ivan Boyukliev <ivanboukliev@abv.bg>
2022-02-09 17:09:34 +0200

    Complete re-factoring of specification-removing group by, order by clauses

--- src/main/java/com/proxiad/hangmangame/logic/statistic/GameStatisticSpecifications.java
+++ src/main/java/com/proxiad/hangmangame/logic/statistic/GameStatisticSpecifications.java
@@ -1,24 +1,19 @@
-package com.proxiad.hangmangame.logic.ranking;
+package com.proxiad.hangmangame.logic.statistic;
 
 import java.sql.Date;
 import java.time.LocalDate;
-import java.util.ArrayList;
-import java.util.List;
 import javax.persistence.criteria.CriteriaBuilder;
 import javax.persistence.criteria.CriteriaQuery;
-import javax.persistence.criteria.Join;
-import javax.persistence.criteria.Order;
 import javax.persistence.criteria.Predicate;
 import javax.persistence.criteria.Root;
 import org.springframework.data.jpa.domain.Specification;
-import com.proxiad.hangmangame.model.GameRanking;
-import com.proxiad.hangmangame.model.GameRanking_;
+import com.proxiad.hangmangame.model.GameResult;
 import com.proxiad.hangmangame.model.GameStatistic;
 import com.proxiad.hangmangame.model.GameStatistic_;
 
-public class GameRankingSpecifications {
+public class GameStatisticSpecifications {
 
-  public static Specification<GameStatistic> havePlayedForTheLastNDays(int days) {
+  public static Specification<GameStatistic> haveWonGamesForLastNDays(int days) {
 
     return new Specification<GameStatistic>() {
 
@@ -26,21 +21,17 @@ public class GameRankingSpecifications {
       public Predicate toPredicate(
           Root<GameStatistic> root, CriteriaQuery<?> query, CriteriaBuilder criteriaBuilder) {
 
-        Join<GameStatistic, GameRanking> gameRanking = root.join(GameStatistic_.GAME_RANKING);
-
         LocalDate localDateXDaysBefore = LocalDate.now().minusDays(days);
         Date today = new Date(System.currentTimeMillis());
         Date dateXDaysAgo = Date.valueOf(localDateXDaysBefore);
 
-        query.groupBy(gameRanking.get(GameRanking_.GAMER_NAME));
+        Predicate predicateForGameWin =
+            criteriaBuilder.equal(root.get(GameStatistic_.GAME_RESULT), GameResult.WIN);
+        Predicate predicateForDate =
+            criteriaBuilder.between(
+                root.get(GameStatistic_.GAME_COMPLETION_DATE), dateXDaysAgo, today);
 
-        List<Order> orderList = new ArrayList<>();
-        orderList.add(criteriaBuilder.desc(gameRanking.get(GameRanking_.TOTAL_WINS)));
-        orderList.add(criteriaBuilder.asc(gameRanking.get(GameRanking_.GAMER_NAME)));
-        query.orderBy(orderList);
-
-        return criteriaBuilder.between(
-            root.get(GameStatistic_.GAME_COMPLETION_DATE), dateXDaysAgo, today);
+        return criteriaBuilder.and(predicateForGameWin, predicateForDate);
       }
     };
   }
