3d2bb225cdc49b1eb59adad0469b77d303e7178d
Ivan Boyukliev <ivanboukliev@abv.bg>
2022-02-11 17:26:33 +0200

    Added new method for retrieving game stat by game session.

--- src/main/java/com/proxiad/hangmangame/logic/statistic/GameStatisticRepository.java
+++ src/main/java/com/proxiad/hangmangame/logic/statistic/GameStatisticRepository.java
@@ -1,10 +1,15 @@
 package com.proxiad.hangmangame.logic.statistic;
 
+import java.util.Optional;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
 import org.springframework.stereotype.Repository;
+import com.proxiad.hangmangame.model.GameSession;
 import com.proxiad.hangmangame.model.GameStatistic;
 
 @Repository
-public interface GameStatisticRepositorty
-    extends JpaRepository<GameStatistic, Long>, JpaSpecificationExecutor<GameStatistic> {}
+public interface GameStatisticRepository
+    extends JpaRepository<GameStatistic, Long>, JpaSpecificationExecutor<GameStatistic> {
+
+  Optional<GameStatistic> findGameStatisticByGameSession(GameSession gameSession);
+}
