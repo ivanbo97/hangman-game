2beb0b1bdb4d93b193087361e19b57dc0a60ba45
Ivan Boyukliev <ivanboukliev@abv.bg>
2022-02-11 10:07:30 +0200

    More words were added.

--- src/main/java/com/proxiad/hangmangame/logic/game/RandomNumberGenerator.java
+++ src/main/java/com/proxiad/hangmangame/logic/game/RandomNumberGenerator.java
@@ -1,7 +1,7 @@
 package com.proxiad.hangmangame.logic.game;
 
 public class RandomNumberGenerator {
-  public static final int MAX_NUMBER = 6;
+  public static final int MAX_NUMBER = 17;
 
   public static final int MIN_NUMBER = 1;
 
