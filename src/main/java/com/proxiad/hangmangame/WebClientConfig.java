9f8263f3eee2553dc3879c03aafeac9f88d4d83f
Ivan Boyukliev <ivanboukliev@abv.bg>
2022-02-25 16:19:20 +0200

    Class for configuration of WebClient.

---
+++ src/main/java/com/proxiad/hangmangame/WebClientConfig.java
@@ -0,0 +1,15 @@
+package com.proxiad.hangmangame;
+
+import org.springframework.boot.web.reactive.function.client.WebClientCustomizer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.hateoas.config.HypermediaWebClientConfigurer;
+
+@Configuration
+public class WebClientConfig {
+
+  @Bean
+  WebClientCustomizer hypermediaWebClientCustomizer(HypermediaWebClientConfigurer configurer) {
+    return configurer::registerHypermediaTypes;
+  }
+}
