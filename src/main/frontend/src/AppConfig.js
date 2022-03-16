bd40e30a2d37523860bb7de027c5716ccb1b8554
Ivan Boyukliev <ivanboukliev@abv.bg>
2022-03-16 18:31:45 +0200

    Added swr config.

--- src/main/frontend/src/AppConfig.js
+++ src/main/frontend/src/AppConfig.js
@@ -1,8 +1,37 @@
 import { Toaster } from "react-hot-toast";
+import { SWRConfig } from "swr";
+
+export const apiFetch = async (url, init) => {
+  const headers = new Headers();
+  headers.append("Accept", "application/json");
+  const response = await fetch(url, {
+    credentials: "same-origin",
+    headers,
+    ...init,
+  });
+
+  if (!response.ok) {
+    const error = new Error("An error occurred while fetching the data.");
+    error.status = response.status;
+
+    const contentType = response.headers.get("Content-Type");
+    if (contentType != null && contentType.indexOf("json") > -1) {
+      error.info = await response.json();
+    }
+
+    throw error;
+  }
+
+  return response.json();
+};
 
 export default function AppConfig({ children }) {
   return (
-    <>
+    <SWRConfig
+      value={{
+        fetcher: apiFetch,
+      }}
+    >
       <Toaster
         gutter={8}
         toastOptions={{
@@ -16,6 +45,6 @@ export default function AppConfig({ children }) {
         }}
       />
       {children}
-    </>
+    </SWRConfig>
   );
 }
