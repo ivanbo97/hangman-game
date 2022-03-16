40bb6d17fd1678d602ac1c513eda8adb5a9a0d4c
Ivan Boyukliev <ivanboukliev@abv.bg>
2022-03-16 18:31:21 +0200

    App is accessed from localhost:8080

--- src/main/frontend/src/setupProxy.js
+++ src/main/frontend/src/setupProxy.js
@@ -4,7 +4,7 @@ module.exports = function (app) {
   app.use(
     ["/api", "/login", "/actuator", "/swagger-ui", "/v3/api-docs"],
     createProxyMiddleware({
-      target: "http://localhost:8080/hangman-game",
+      target: "http://localhost:8080",
       changeOrigin: true,
       xfwd: true,
     })
