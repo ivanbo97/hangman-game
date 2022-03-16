2e5ff8c23afee232ec265667f654fe75df723bd6
Ivan Boyukliev <ivanboukliev@abv.bg>
2022-03-16 18:33:02 +0200

    Removed fetcher, now placed in appconfig component

--- src/main/frontend/src/app/api/RankingApi.js
+++ src/main/frontend/src/app/api/RankingApi.js
@@ -1,6 +1,5 @@
 import useSWR from "swr";
 
-const fetcher = (url) => fetch(url).then((res) => res.json());
 const top10PlayersEverUrl = "/api/v1/rankings/top10";
 const top10PlayersLastMonthUrl = "/api/v1/rankings/top10-last-month";
 
@@ -8,7 +7,7 @@ export function useRanking(shouldDisplayTop10EverList) {
   const apiUrl = shouldDisplayTop10EverList
     ? top10PlayersEverUrl
     : top10PlayersLastMonthUrl;
-  return useSWR(apiUrl, fetcher, {
+  return useSWR(apiUrl, {
     refreshInterval: 3 * 60 * 1000,
   });
 }
